acf30d05bcfbb96a11f18e2b3d17c1848e474334
Bppatkar <bhanupratappatkar777@gmail.com>
2024-05-22 19:09:01 +0530

    half work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
+import React from "react";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Single from "./pages/Single";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div>
-      <h1>bhanu</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Chart.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import "./chart.css";
+
+const data = [
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "March", Total: 800 },
+  { name: "April", Total: 1600 },
+  { name: "May", Total: 900 },
+  { name: "June", Total: 1700 },
+];
+
+const Chart = () => {
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">Sales Analytics</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <Tooltip />
+          <Line type="monotone" dataKey="Total" stroke="#f28070" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
